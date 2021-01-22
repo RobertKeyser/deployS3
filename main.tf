6721a5f1bdc84fc610d008ef125059a2013b8dbb
Robert Keyser <39230492+RobertKeyser@users.noreply.github.com>
2021-01-22 11:10:52 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = var.bucket_name
+  acl    = "private"
+}
